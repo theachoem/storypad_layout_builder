ddd7ad71481942f2cc02c83c88b47cfc9564a463
theachoem <29684683+theachoem@users.noreply.github.com>
2025-04-27 07:40:58 +0000

    Deploying to gh-pages from @ theachoem/storypad_layout_builder@5da01c02e6b2c7248c457a5887b907f90b7bc8fb 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3600796334"
+    serviceWorkerVersion: "1887913638"
   }
 });
