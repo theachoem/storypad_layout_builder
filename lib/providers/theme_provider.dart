9c1d004ab3e7d0178cb38be756a8b65a1cf1c86b
Thea Choem <29684683+theachoem@users.noreply.github.com>
2025-04-28 01:22:38 +0700

    Persist theme to local storage

--- lib/providers/theme_provider.dart
+++ lib/providers/theme_provider.dart
@@ -1,23 +1,48 @@
 import 'package:flutter/material.dart';
 
+// ignore: depend_on_referenced_packages
+import 'package:web/web.dart' as web;
+
 class ThemeProvider extends ChangeNotifier {
   ThemeMode themeMode = ThemeMode.system;
   Color colorSeed = Colors.black;
   FontWeight fontWeight = FontWeight.normal;
   String fontFamily = "Quicksand";
 
+  ThemeProvider() {
+    final themeModeRaw = web.window.localStorage.getItem('themeMode');
+    final fontFamilyRaw = web.window.localStorage.getItem('fontFamily');
+    final colorSeedRaw = web.window.localStorage.getItem('colorSeed');
+
+    for (var e in ThemeMode.values) {
+      if (e.name == themeModeRaw) themeMode = e;
+    }
+
+    fontFamily = fontFamilyRaw ?? fontFamily;
+    colorSeed = colorSeedRaw != null && int.tryParse(colorSeedRaw) != null
+        ? Color(int.parse(colorSeedRaw))
+        : colorSeed;
+  }
+
   void setThemeMode(ThemeMode mode) {
     themeMode = mode;
     notifyListeners();
+
+    web.window.localStorage.setItem('themeMode', mode.name);
   }
 
   void setFontFamily(String font) {
     fontFamily = font;
     notifyListeners();
+
+    web.window.localStorage.setItem('fontFamily', fontFamily);
   }
 
   void setColorSeed(Color value) {
     colorSeed = value;
     notifyListeners();
+
+    // ignore: deprecated_member_use
+    web.window.localStorage.setItem('colorSeed', value.toARGB32().toString());
   }
 }
